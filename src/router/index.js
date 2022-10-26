cd62e4bd643e2a3506226ee5d45d8c699608dc17
logicwang <1758555877@qq.com>
2022-10-27 00:11:38 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import { createRouter,createWebHashHistory } from 'vue-router';
-import LeftList from '../view/leftlist.vue' 
+import leftlist from '../view/leftlist.vue' 
 import NotFound from '../view/NotFound.vue'
 
-// #createRouter创建路由
 const router = createRouter({
     history:createWebHashHistory(),
-    //用哈希模式
     routes:[
         {
-            path:'/', 
-            component:LeftList //引入的components单体文件
+            path:'/',
+            component:leftlist
         },
         {
             path: '/404',
