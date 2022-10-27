4af0de4caceafe7dc328ad612ea4f6a269874020
logicwang <1758555877@qq.com>
2022-10-27 23:35:03 +0800

    优化build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  chainWebpack: config => {
+    const CompressionWebpackPlugin = require('compression-webpack-plugin')
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('CompressionPlugin').use(
+        new CompressionWebpackPlugin({
+          test: /\.(js|css)$/,
+          threshold: 10240, // 超过10kb的文件就压缩
+          deleteOriginalAssets: true, // 不删除源文件
+          minRatio: 0.8
+        })
+      )
+    }
+  }
 })
