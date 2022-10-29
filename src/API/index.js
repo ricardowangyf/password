bcd688671ec2ae10e4d54ac0a225339fbd9148f7
logicwang <1758555877@qq.com>
2022-10-29 18:18:00 +0800

    修改API名称

--- src/API/index.js
+++ src/API/index.js
@@ -1,3 +1,3 @@
 import requests from './request'
 
-export const leftlist = () => requests({ url: 'http://localhost:3721/api/user/leftlist', method: 'GET' }) //中间路由
+export const reqCategoryList = () => requests({ url: 'http://localhost:3721/api/user/list', method: 'GET' }) 
