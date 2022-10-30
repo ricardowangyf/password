a6f52bc22ea984d240b8c83c159bb731e2660cb6
logicwang <1758555877@qq.com>
2022-10-30 19:28:02 +0800

    添加新 API

--- src/API/index.js
+++ src/API/index.js
@@ -1,3 +1,18 @@
+//index.js
 import requests from './request'
+//例如
+//三级联动接口
+//接口地址  请求方式  有无参数 ----注释
+//——get 无参
 
-export const reqCategoryList = () => requests({ url: 'http://localhost:3721/api/user/list', method: 'GET' }) 
+
+export const reqCategoryList = () => requests({ url: 'http://localhost:3721/api/user/list', method: 'GET' }) //中间路由
+
+export const save = (paramssa) => requests({ url: 'http://localhost:3721/api/user/list/save', method: 'POST',data:{...paramssa }}) 
+
+export const details = (params) => requests({ url: 'http://localhost:3721/api/user/list/details', method: 'POST', data:{...params}}) //右侧详情页
+
+//——post 有参
+//params至少是一个空对象
+
+// export const reqGetSearchInfo = (params) => requests({ url: '不带/api', method: 'post', data: params })
