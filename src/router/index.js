cd4256cd8933d520ce8496130c6373a92664339e
logicwang <1758555877@qq.com>
2022-11-10 10:25:32 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import leftlist from '../view/leftlist.vue' //菜单列表
 import NotFound from '../view/NotFound.vue' //404页面
 import middlelist from '../view/middlelist.vue' //列表页面
 import DetailPage from '../view/DetailPage.vue' //详情页
+import RouterList from '../components/RouterList.vue' //路由内容
 
 
 const router = createRouter({
@@ -16,18 +17,24 @@ const router = createRouter({
       component: leftlist,
       children: [
         {
-          path: 'list/:type',
+          path: '/list/:type',
           component: middlelist,
           props: true
         },
         {
-          path: 'list/:type/detail/:name',
+          path: '/list/:type/detail/:name',
           component: middlelist,
           props: true
         },
         {
-            path: 'DetailPage',
-            component: DetailPage,
+          path: '/DetailPage',
+          name: '/DetailPage',
+          component: DetailPage,
+        },
+        {
+          path: '/RouterList',
+          name: '/RouterList',
+          component: RouterList,
         },
       ]
     },
