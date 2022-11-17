046ae37bc7a47efd4e2ca023c8393afa65ef5f8e
logicwang <1758555877@qq.com>
2022-11-17 23:21:52 +0800

    删除在indexjs中的console.log内容

--- mocker/detalis.js
+++ mocker/detalis.js
@@ -5,11 +5,8 @@ const { list }= require('./data')
 const proxy = {
   'POST /api/user/list/details': (req, res) => {
     const  name  = req.body.name;
-    console.log('---name-->',name)
     const items = name && list.filter(item => (item.name === name));
-    console.log('----items--->', items)
     const data = items && items.length > 0 ? items[0] : {}
-    console.log('---data---->',data)
     return res.json(data);
   },
 }
